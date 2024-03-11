1aad995215218b8a97d4e3ff4eed72d0e81214b9
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-03-11 23:20:51 +0000

    Update BlockCypherAPIClient.cs

--- Services/BlockCypherAPIClient.cs
+++ Services/BlockCypherAPIClient.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using RestSharp;
+
+//Implement coinGecko APi
+namespace Test_VentiMM.Sources
+{
+    class BlockCypherAPIClient
+    {
+        private string apiBaseUrl = "";
+        public string GetLitcoinWallet()
+        {
+            string Api_URL = "https://api.blockcypher.com/v1/example_endpoint?token=" + apiToken;
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(Api_URL).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = response.Content.ReadAsStringAsync().Result;
+                return responseContent;
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                return null;
+            }
+        }
+
+    }
+}
 
